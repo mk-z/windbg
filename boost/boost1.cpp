8db5f851d3dfc97a378acf7d367388231f2e88e9
mk-z <zyf079@gmail.com>
2016-11-30 10:14:46 +0800

    boost study

---
+++ boost/boost1.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "hello" << endl;
+    getchar();
+    return 0;
+}
