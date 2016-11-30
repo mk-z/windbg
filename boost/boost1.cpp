3b75914a44a0c9500a03ec5e58284886e8f02ca2
mk-z <zyf079@gmail.com>
2016-11-30 11:36:19 +0800

    test1

--- boost/boost1.cpp
+++ boost/boost1.cpp
@@ -6,11 +6,17 @@
 #include <algorithm>
 #include <stdio.h>
 
+#include <boost/version.hpp>
+#include <boost/config.hpp>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    cout << "hello" << endl;
+    cout << BOOST_VERSION << endl;
+    cout << BOOST_LIB_VERSION << endl;
+    cout << BOOST_PLATFORM << endl;
+    cout << BOOST_COMPILER  << endl;
+    cout << BOOST_STDLIB << endl;
     getchar();
     return 0;
 }
