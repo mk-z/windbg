7591292bd7d25a45a5faf7a1615496dc2a2d9dc9
mk-z <zyf079@gmail.com>
2016-12-12 14:01:30 +0800

    缓存

--- python/spider/link_crawler.py
+++ python/spider/link_crawler.py
@@ -5,12 +5,12 @@ import re
 import urlparse
 import urllib2
 import time
-from datetime import datetime
+import datetime
 import robotparser
-import Queue
+from download import Downloader
 
 
-def link_crawler(seed_url, link_regex=None, delay=5, max_depth=-1, max_urls=-1, headers=None, user_agent='wswp', proxy=None, num_retries=1, scrape_callback=None):
+def link_crawler(seed_url, link_regex=None, delay=5, max_depth=-1, max_urls=-1, user_agent='wswp', proxies=None, num_retries=1, scrape_callback=None, cache=None):
     """Crawl from the given seed URL following links matched by link_regex
     """
     # the queue of URL's that still need to be crawled
@@ -20,18 +20,14 @@ def link_crawler(seed_url, link_regex=None, delay=5, max_depth=-1, max_urls=-1, 
     # track how many URL's have been downloaded
     num_urls = 0
     rp = get_robots(seed_url)
-    throttle = Throttle(delay)
-    headers = headers or {}
-    if user_agent:
-        headers['User-agent'] = user_agent
+    D = Downloader(delay=delay, user_agent=user_agent, proxies=proxies, num_retries=num_retries, cache=cache)
 
     while crawl_queue:
         url = crawl_queue.pop()
         depth = seen[url]
         # check url passes robots.txt restrictions
         if rp.can_fetch(user_agent, url):
-            throttle.wait(url)
-            html = download(url, headers, proxy=proxy, num_retries=num_retries)
+            html = D(url)
             links = []
             if scrape_callback:
                 links.extend(scrape_callback(url, html) or [])
@@ -59,51 +55,6 @@ def link_crawler(seed_url, link_regex=None, delay=5, max_depth=-1, max_urls=-1, 
         else:
             print 'Blocked by robots.txt:', url
 
-
-class Throttle:
-    """Throttle downloading by sleeping between requests to same domain
-    """
-    def __init__(self, delay):
-        # amount of delay between downloads for each domain
-        self.delay = delay
-        # timestamp of when a domain was last accessed
-        self.domains = {}
-        
-    def wait(self, url):
-        """Delay if have accessed this domain recently
-        """
-        domain = urlparse.urlsplit(url).netloc
-        last_accessed = self.domains.get(domain)
-        if self.delay > 0 and last_accessed is not None:
-            sleep_secs = self.delay - (datetime.now() - last_accessed).seconds
-            if sleep_secs > 0:
-                time.sleep(sleep_secs)
-        self.domains[domain] = datetime.now()
-
-
-
-def download(url, headers, proxy, num_retries, data=None):
-    print 'Downloading:', url
-    request = urllib2.Request(url, data, headers)
-    opener = urllib2.build_opener()
-    if proxy:
-        proxy_params = {urlparse.urlparse(url).scheme: proxy}
-        opener.add_handler(urllib2.ProxyHandler(proxy_params))
-    try:
-        response = opener.open(request)
-        html = response.read()
-        code = response.code
-    except urllib2.URLError as e:
-        print 'Download error:', e.reason
-        html = ''
-        if hasattr(e, 'code'):
-            code = e.code
-            if num_retries > 0 and 500 <= code < 600:
-                # retry 5XX HTTP errors
-                html = download(url, headers, proxy, num_retries-1, data)
-        else:
-            code = None
-    return html
 
 
 def normalize(seed_url, link):
