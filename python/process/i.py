6ad3e2e353d93121beb1722e0ace43ae950caf62
mk-z <zyf079@gmail.com>
2017-01-22 17:12:39 +0800

    process 1

---
+++ python/process/i.py
@@ -0,0 +1,25 @@
+import multiprocessing
+import time
+
+def daemon():
+    while 1:
+        time.sleep(10)
+
+def non_daemon():
+    while 1:
+        time.sleep(100)
+
+def non_daemon_break():
+    time.sleep(3)      #让他主动退出
+
+if __name__ == '__main__':
+    f = []
+    f.append(multiprocessing.Process(name='daemon', target=daemon))
+    f.append(multiprocessing.Process(name='non-daemon', target=non_daemon))
+    f.append(multiprocessing.Process(name='non-daemon', target=non_daemon_break))
+
+    for i in f:
+        i.daemon = True
+        i.start()
+        while 1:
+            time.sleep(1)
