5132e39ad02e9b206363fe25a56268c5457d4b9c
mk-z <zyf079@gmail.com>
2017-02-16 17:35:39 +0800

    py3

---
+++ python/py3/scraper_thread.py
@@ -0,0 +1,17 @@
+import time
+import requests
+from concurrent.futures import ThreadPoolExecutor
+
+NUMBERS = range(12)
+URL = 'http://httpbin.org/get?a={}'
+
+def fetch(a):
+    r = requests.get(URL.format(a))
+    return r.json()['args']['a']
+
+start = time.time()
+with ThreadPoolExecutor(max_workers=3) as executor:
+    for num, result in zip(NUMBERS, executor.map(fetch, NUMBERS)):
+        print('fetch({}) = {}'.format(num, result))
+
+print('Use requests+ThreadPoolExecutor cost: {}'.format(time.time()-start))
